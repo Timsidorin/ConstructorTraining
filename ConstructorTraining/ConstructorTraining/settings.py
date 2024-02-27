a0d9227b496061225bc323ccac36612690c673aa
B2ng4 <125038202+B2ng4@users.noreply.github.com>
2024-02-27 18:42:06 +1000

    new version
    Добавление авторизации

--- ConstructorTraining/ConstructorTraining/settings.py
+++ ConstructorTraining/ConstructorTraining/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Application definition
 
@@ -38,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'users'
 ]
 
 MIDDLEWARE = [
--- ConstructorTraining/ConstructorTraining/urls.py
+++ ConstructorTraining/ConstructorTraining/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path
-from  main import views
+from django.urls import path, include
 from main.views import index_page
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_page)
+    path('choose/', index_page),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    path('users/', include('users.urls')),
 ]
